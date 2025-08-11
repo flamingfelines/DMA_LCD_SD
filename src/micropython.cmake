353630f7f9f383b119d51f7a842af03b061951c3
flamingfelines <soulareflare@gmail.com>
2025-08-11 16:33:21 -0700

    Update micropython.cmake
    update to include new esp_spi and esp_sd files

--- src/micropython.cmake
+++ src/micropython.cmake
@@ -6,6 +6,8 @@ target_sources(usermod_s3lcd INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/s3lcd.c
     ${CMAKE_CURRENT_LIST_DIR}/s3lcd_i80_bus.c
     ${CMAKE_CURRENT_LIST_DIR}/s3lcd_spi_bus.c
+    ${CMAKE_CURRENT_LIST_DIR}/esp_spi.c
+    ${CMAKE_CURRENT_LIST_DIR}/esp_sd.c
     ${CMAKE_CURRENT_LIST_DIR}/mpfile.c
     ${CMAKE_CURRENT_LIST_DIR}/jpg/tjpgd565.c
     ${CMAKE_CURRENT_LIST_DIR}/png/pngle.c
@@ -16,13 +18,16 @@ target_sources(usermod_s3lcd INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/pngenc/pngenc.c
     ${CMAKE_CURRENT_LIST_DIR}/pngenc/trees.c
     ${CMAKE_CURRENT_LIST_DIR}/pngenc/zutil.c
-    )
+)
 
 # Add the current directory as an include directory.
 target_include_directories(usermod_s3lcd INTERFACE
     ${IDF_PATH}/components/esp_lcd/include/
+    ${IDF_PATH}/components/driver/spi/include/
+    ${IDF_PATH}/components/sdmmc/include/
+    ${IDF_PATH}/components/fatfs/vfs/
     ${CMAKE_CURRENT_LIST_DIR}
-    )
+)
 
 # Link our INTERFACE library to the usermod target.
 target_link_libraries(usermod INTERFACE usermod_s3lcd)
