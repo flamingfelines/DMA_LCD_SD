e8c7ad94c46620abaed1ef6b0cdc84df5e52ef62
flamingfelines <soulareflare@gmail.com>
2025-08-11 16:19:00 -0700

    Update esp_spi.h

--- src/esp_spi.h
+++ src/esp_spi.h
@@ -5,31 +5,30 @@
 #include "driver/spi_master.h"
 
 // SPIBus object struct
-typedef struct _esp_spi_bus_obj_t {
+typedef struct esp_spi_bus_obj_t {
     mp_obj_base_t base;
     int miso_io_num;
     int mosi_io_num;
     int sclk_io_num;
     spi_host_device_t host;  // e.g. SPI2_HOST
-
     bool initialized;
 } esp_spi_bus_obj_t;
 
 extern const mp_obj_type_t esp_spi_bus_type;
 
-mp_obj_t esp_spi_bus_make_new(const mp_obj_type_t *type,
-                             size_t n_args, size_t n_kw,
-                             const mp_obj_t *args);
-
-mp_obj_t esp_spi_bus_init(mp_obj_t self_in);
-
-// Device object struct
-typedef struct _esp_spi_device_obj_t {
+// Device object struct  
+typedef struct esp_spi_device_obj_t {
     mp_obj_base_t base;
     spi_device_handle_t spi_dev_handle;
 } esp_spi_device_obj_t;
 
-// Add device function
+extern const mp_obj_type_t esp_spi_device_type;
+
+// Function declarations
+mp_obj_t esp_spi_bus_make_new(const mp_obj_type_t *type,
+                             size_t n_args, size_t n_kw,
+                             const mp_obj_t *args);
+mp_obj_t esp_spi_bus_init(mp_obj_t self_in);
 mp_obj_t esp_spi_bus_add_device(size_t n_args, const mp_obj_t *args, mp_map_t *kw_args);
 
 #endif // ESP_SPI_H
