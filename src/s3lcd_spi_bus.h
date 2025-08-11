a4c094dc1a4b8370be805ed7d6e08017911ed8cc
flamingfelines <soulareflare@gmail.com>
2025-08-10 20:22:05 -0700

    Update s3lcd_spi_bus.h

--- src/s3lcd_spi_bus.h
+++ src/s3lcd_spi_bus.h
@@ -22,44 +22,41 @@
 
 #ifndef __s3lcd_spi_bus_H__
 #define __s3lcd_spi_bus_H__
-
 #include "mphalport.h"
 #include "py/obj.h"
 #include "esp_lcd_panel_io.h"
 #include "driver/spi_master.h"
 #include "extmod/modmachine.h" // for mp_machine_spi_p_t
 
-// spi Configuration
-
+// SPI Bus Configuration Structure
 typedef struct _s3lcd_spi_bus_obj_t {
     mp_obj_base_t base;                     // base class
     char *name;                             // name of the display
-    mp_obj_t spi_obj;               // NEW: The Python machine.SPI object we attach to
-    spi_device_handle_t spi_dev;    // NEW: The handle for this display device
-
-    int spi_host;                           // SPI host
-    int sclk_io_num;                        // GPIO used for SCLK line
-    int mosi_io_num;                        // GPIO used for MOSI line
-    int miso_io_num;                        // GPIO used for MISO line
-    int quadwp_io_num;                      // GPIO used for QuadWP line
-    int quadhd_io_num;                      // GPIO used for QuadHD line
-    int cs_gpio_num;                        // GPIO used for CS line */
-    int dc_gpio_num;                        // GPIO used to select the D/C line, set this to -1 if the D/C line not controlled by manually pulling high/low GPIO */
-    int spi_mode;                           // Traditional SPI mode (0~3) */
-    unsigned int pclk_hz;                   // Frequency of pixel clock */
-    size_t trans_queue_depth;               // Size of internal transaction queue */
-    int lcd_cmd_bits;                       // Bit-width of LCD command */
-    int lcd_param_bits;                     // Bit-width of LCD parameter */
-    struct {                                // Extra flags to fine-tune the SPI device
+    mp_obj_t spi_obj;                       // The Python machine.SPI object we attach to
+    spi_device_handle_t spi_dev;            // The handle for this display device on the SPI bus
+    
+    // GPIO configuration
+    int cs_gpio_num;                        // GPIO used for CS line
+    int dc_gpio_num;                        // GPIO used to select the D/C line, set this to -1 if the D/C line not controlled by manually pulling high/low GPIO
+    
+    // SPI configuration parameters
+    int spi_mode;                           // Traditional SPI mode (0~3)
+    unsigned int pclk_hz;                   // Frequency of pixel clock
+    int lcd_cmd_bits;                       // Bit-width of LCD command
+    int lcd_param_bits;                     // Bit-width of LCD parameter
+    
+    // Flags for SPI behavior
+    struct {
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 0, 0)
         unsigned int dc_as_cmd_phase: 1;    // D/C line value is encoded into SPI transaction command phase
+#endif
         unsigned int dc_low_on_data: 1;     // If this flag is enabled, DC line = 0 means transfer data, DC line = 1 means transfer command; vice versa
         unsigned int octal_mode: 1;         // transmit with octal mode (8 data lines), this mode is used to simulate Intel 8080 timing
-        unsigned int lsb_first: 1;          // transmit LSB bit first */
-        unsigned int swap_color_bytes:1;    // Swap color bytes in 16-bit color mode */
+        unsigned int lsb_first: 1;          // transmit LSB bit first
+        unsigned int swap_color_bytes: 1;   // Swap color bytes in 16-bit color mode
     } flags;
-
 } s3lcd_spi_bus_obj_t;
 
 extern const mp_obj_type_t s3lcd_spi_bus_type;
 
-#endif /* __spi_bus_H__ */
+#endif /* __s3lcd_spi_bus_H__ */
