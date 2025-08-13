8459a734b08f8b92228bfc398ce35e22e576e127
flamingfelines <soulareflare@gmail.com>
2025-08-12 19:54:14 -0700

    Update esp_sd.h

--- src/esp_sd.h
+++ src/esp_sd.h
@@ -1,7 +1,10 @@
 #ifndef ESP_SD_H
 #define ESP_SD_H
+
 #include "py/obj.h"
 #include "driver/sdmmc_types.h"
+#include <stdbool.h>
+
 // SD Card object struct
 typedef struct esp_sd_obj_t {
     mp_obj_base_t base;
@@ -9,10 +12,13 @@ typedef struct esp_sd_obj_t {
     char *mount_point;
     mp_obj_t bus;           
     int cs_pin;            
+    bool mounted;           // Added: missing mounted field
 } esp_sd_obj_t;
+
 // Type declaration
 extern const mp_obj_type_t esp_sd_type;
 
 // Module declaration
 extern const mp_obj_module_t esp_sd_module;
+
 #endif // ESP_SD_H
